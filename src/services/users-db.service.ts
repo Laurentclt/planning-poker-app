cc12da87abda6c12048eda272836ca80b96fa1a7
Laurent Collet <Laurentpro.collet@gmail.com>
2022-09-22 00:47:05 +0200

    bug fix data missing for user who doesn't create game

--- src/services/users-db.service.ts
+++ src/services/users-db.service.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { Player } from '../app/models/player.model';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import { Collection } from 'src/app/models/collection.model';
+import { Router } from '@angular/router';
 
 
 
@@ -20,9 +21,8 @@ export class UsersDbService {
   gameSessionsCollection: AngularFirestoreCollection<Collection>;
   currentGameSessionId: string;
 
-  constructor(afs: AngularFirestore) {
+  constructor(afs: AngularFirestore,  private router : Router ) {
     this.gameSessionsCollection = afs.collection<Collection>('game-sessions')
-    this.players = afs.collection<Collection>('game-sessions').doc(this.currentGameSessionId).collection<Player>('players').valueChanges()
     this.afs = afs
   }
   createGameSession(): string {
@@ -30,16 +30,26 @@ export class UsersDbService {
     const gameSessionId: Collection = {id}
     this.gameSessionsCollection.doc(id).set(gameSessionId)
     this.currentGameSessionId = id
+    this.setPlayers(id)
     return id
+  }
+  setPlayers(id: string) {
+    this.players = this.afs.collection<Collection>('game-sessions')
+    .doc(id).collection<Player>('players').valueChanges()
   }
 
   addUser(playerName: string): void {
     // to do : first player created in the collection is admin
+    if (this.currentGameSessionId === undefined) {
+      const id = this.router.url
+      this.currentGameSessionId = id
+      this.setPlayers(id)
+    }
+    console.log('adduser called', playerName)
     const playerId = this.afs.createId();
     const newPlayer: Player = { id: playerId, name: playerName, admin: this.isAdmin, cardValue: null };
     this.currentUser = newPlayer
     this.gameSessionsCollection.doc(this.currentGameSessionId).collection<Player>('players').doc(playerId).set(newPlayer)
-    
   }
   
   updateUserCard(id: string, value: number): void {
@@ -48,7 +58,14 @@ export class UsersDbService {
 
   deleteUser(id: string): void {
     this.gameSessionsCollection.doc(this.currentGameSessionId).collection('players').doc(id).delete()
+    let playersPlaying: number;
+    this.players.subscribe(data => {
+      playersPlaying = data.length
+      if (playersPlaying === 0) {
+        console.log("supression de la collection")
+        this.gameSessionsCollection.doc(this.currentGameSessionId).delete()
+      }
+    } )
   }
 
-  
 }
